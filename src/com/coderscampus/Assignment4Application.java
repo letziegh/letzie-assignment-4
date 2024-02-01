11f16aae5cc9620884ada5ba626e2e2994aed143
letziegh <148257470+letziegh@users.noreply.github.com>
2024-02-01 10:43:48 -0500

    Final Update to Assignment 4

--- src/com/coderscampus/Assignment4Application.java
+++ src/com/coderscampus/Assignment4Application.java
@@ -10,137 +10,25 @@ import java.util.Arrays;
 
 public class Assignment4Application {
 
-	/*
-	 * 1. Create java project 2. create main 3. create POJO 4. Download master list
-	 * file 5. parse and Separate data into 3 CSV files 6. Group Students by course
-	 * 7. Once separated sort the students by grade in descending order The CSV
-	 * output file names should be called: course1.csv, course2.csv, and
-	 * course3.csv.
-	 * 
-	 * HINTS
-	 * 
-	 * In order to sort an array, you'll need to figure out how to handle null
-	 * entries in your array. If you're trying to use Arrays.sort(yourArray), and
-	 * yourArray contains null values, the sort will crash. When writing to a file,
-	 * you can use "\n" to write a new line to the file. Fore example:
-	 * fileWriter.write("This is one line \n"); If you want to turn String input
-	 * into an Integer, you can parse it like so: Integer myIntVal =
-	 * Integer.parseInt(myStringVal);
-	 * 
-	 */
-	// FIRST GET THE READER TO READ CSV FILE THEN PARSE INTO 3 DIFFERENT
-	// FILES--DONE!!!!COMPLETED!!!just added C://
-//	parse by class(compsci, stat, apmth) put each into an array
-	// and Separate data into 3 CSV files; use bufferedwriter to read 3 arrays into
-	// 3 different files
-	// Watch video on looping \n to start a new line for each student in file to
-
-	// Use this. to sort
-	// then save into 3 separate csv files
+	
+	
 	public static void main(String[] args) throws IOException {
 		FileService fileService = new FileService();
-		Enrollment[] enrollments = fileService.getStudentsFromFile();
+		
 		
 		
 		String outputFile1 = "course1.csv";
 		String outputFile2 = "course2.csv";
 		String outputFile3 = "course3.csv";
-		
+		//System.out.println(Arrays.toString(fileService.sortedStudentsInFileOne()));
 		fileService.writeToAFile(outputFile1,fileService.sortedStudentsInFileOne());	
 		fileService.writeToAFile(outputFile2,fileService.sortedStudentsInFileTwo());	
 		fileService.writeToAFile(outputFile3,fileService.sortedStudentsInFileThree());	
 		
 		
-		
-
-//		try {
-//			writer1 = new BufferedWriter(new FileWriter(outputFile1));
-//			writer1.write(fileService.sortedStudentsInFileOne());
-//			writer2 = new BufferedWriter(new FileWriter(outputFile2));
-//			writer3 = new BufferedWriter(new FileWriter(outputFile3));
-//		} finally {
-//			if (writer1 != null)writer1.close();
-//			if(writer2 !=null)writer2.close();
-//			if(writer3 !=null)writer3.close();
-//			// String line="";
-//			// while ((line = reader.readLine()) != null) {
-//			// writer.write(fileService.sortedStudentsInFileOne());
-//		}
-//		
-		
-		//fileService.sortedStudentsInFileOne();
-		//fileService.sortedStudentsInFileTwo();
-		//fileService.sortedStudentsInFileThree();
-
-		// have loop for each course
-		// sort by grade
-		// use buffered writer to read into file
-		// create method for buffered writer
-//		for (Enrollment apmth : enrollments) {
-//			if (apmth.getCourse().contains("APMTH"))
-//				System.out.println(apmth);
-//
-//		}
-//		System.out.println("---------");
-//		Arrays.sort(enrollments);
-//		for (Enrollment compsci : enrollments) {
-//			if (compsci.getCourse().contains("COMPSCI")) {
-//				System.out.println(compsci);
-//			}
-//		}System.out.println("---------");
-//				// use buffered writer to read this into file after
-//				// sorting by grade
-//
-//		for(Enrollment stat:enrollments) {
-//			if(stat.getCourse().contains("STAT")) {
-//				System.out.println(stat);
-//			}
-//		}
-		
-
-		// BEFORE SORTING MUST SPLIT FILE BASED ON COURSE, THEN SORT BY GRADE IN
-		// DESCENDING ORDER
-		// Arrays.sort(studentCourses);
-//		for(String studentCourse: studentCourses) {
-//			System.out.println(studentCourse);
-
-//		}
+	
 
 	}
 }
 
-//		String path = "C:\\Users\\abbah\\OneDrive\\Desktop\\student-master-list.csv";
-//		try {
-//			BufferedReader reader = new BufferedReader(new FileReader(path));
-//			int i= 0;
-//			Enrollment[] enrollments = new Enrollment [101];
-//			String line = "";
-//			while ((line = reader.readLine()) != null) {
-//				
-//				String[] student = line.split(","); //change from "," to Course; change from array to string to read values
-//				String iD= student[0];
-//				//int idInt = Integer.parseInt(iD);
-//				String name = student[1];
-//				String course = student[2];
-//				String grade = student[3];
-//				//int grade = Integer.parseInt(student[3]);
-//				Enrollment enrollment= new Enrollment(iD, name, course, grade);
-//				enrollments[i] = enrollment;
-//				//Courses c= new Courses(student[0], student[1], student [2], student[3]);
-//				//student [i] = new students;
-//				i++;
-//				System.out.println(enrollment);
-//				//Arrays.toString(enrollments)
-//				
-//				
-//			}
-//				//insert loop with string to read arrays
-//				//System.out.println(values);
-//
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}//return enrollments;
-//
-//	
+
--- src/com/coderscampus/FileService.java
+++ src/com/coderscampus/FileService.java
@@ -12,140 +12,106 @@ import java.util.Comparator;
 
 public class FileService {
 
-	//public void writeStudentsToFile(Object BufferedWriter, BufferedWriter writer1, BufferedWriter writer2, BufferedWriter writer3) throws IOException {
-		//FileService fileService = new FileService();
-		//BufferedWriter  writer1 = new BufferedWriter();
+	public void writeToAFile(String fileName, Enrollment[] enrollments) throws IOException {
 
-//		String outputFile1 = "course1.csv";
-//		String outputFile2 = "course2.csv";
-//		String outputFile3 = "course3.csv";
-//
-//		BufferedWriter = null;
-//
-//		try {
-//			writer1 = new BufferedWriter(new FileWriter(outputFile1));
-//			writer1.write(fileService.sortedStudentsInFileOne());
-//			writer2 = new BufferedWriter(new FileWriter(outputFile2));
-//			writer3 = new BufferedWriter(new FileWriter(outputFile3));
-//		} finally {
-//			if (writer1 != null)writer1.close();
-//			if(writer2 !=null)writer2.close();
-//			if(writer3 !=null)writer3.close();
-//			// String line="";
-//			// while ((line = reader.readLine()) != null) {
-//			// writer.write(fileService.sortedStudentsInFileOne());
-//		}
-//	}
+		BufferedWriter writer1 = new BufferedWriter(new FileWriter(fileName));
 
+		try {
 
-	
-public void writeToAFile(String fileName, Enrollment[] enrollments)throws IOException{
-	
-	BufferedWriter writer1 = new BufferedWriter(new FileWriter(fileName)); 
-	
-	try {
-		
-		writer1.write("Student ID, Student Name, Course, Grade \n");
-		for (Enrollment e: enrollments) {
-			writer1.write(e.getiD() + " " + e.getName() + " " + e.getCourse() + " " + e.getGrade() + "\n");
+			writer1.write("Student ID, Student Name, Course, Grade \n");
+			for (Enrollment e : enrollments) {
+				writer1.write(e.getiD() + " " + e.getName() + " " + e.getCourse() + " " + e.getGrade() + "\n");
+			}
+		} finally {
+			if (writer1 != null)
+				writer1.close();
+
 		}
-	}finally{
-		if(writer1 !=null)writer1.close();
-		
 	}
-}
-	
+
 	public Enrollment[] sortedStudentsInFileOne() {
-		int counter= 0;
+		int counter = 0;
 		FileService fileService = new FileService();
 		Enrollment[] enrollments = fileService.getStudentsFromFile();
 		Enrollment[] finalEnrollment = new Enrollment[33];
-		
-		Arrays.sort(enrollments,new Comparator<Enrollment>() {
+
+		Arrays.sort(enrollments, new Comparator<Enrollment>() {
 
 			@Override
 			public int compare(Enrollment o1, Enrollment o2) {
-				
-				return o1.getGrade().compareTo(o2.getGrade());
-			}
-			
-		});
-		
-		
-		
-		
 
-		for (int i = 0; i<enrollments.length;i++) {
+				return o2.getGrade().compareTo(o1.getGrade());
+			}
+
+		});
+
+		for (int i = 0; i < enrollments.length; i++) {
 			Enrollment enrollment = enrollments[i];
 			if (enrollment.getCourse().contains("APMTH")) {
 				finalEnrollment[counter] = enrollment;
 				counter++;
-				
-				
+
 			}
-				
+
 		}
 		return finalEnrollment;
 
 	}
 
 	public Enrollment[] sortedStudentsInFileTwo() {
-		int counter= 0;
+		int counter = 0;
 		FileService fileService = new FileService();
 		Enrollment[] enrollments = fileService.getStudentsFromFile();
-		Enrollment[] finalEnrollment = new Enrollment[35];
-		
-	//	Arrays.sort(enrollments);
-		Arrays.sort(enrollments,new Comparator<Enrollment>() {
+		Enrollment[] finalEnrollment = new Enrollment[34];
+
+		// Arrays.sort(enrollments);
+		Arrays.sort(enrollments, new Comparator<Enrollment>() {
 
 			@Override
 			public int compare(Enrollment o1, Enrollment o2) {
-				
-				return o1.getGrade().compareTo(o2.getGrade());
-			}
-			
-		});
-		
 
-		for (int i = 0; i<enrollments.length;i++) {
+				return o2.getGrade().compareTo(o1.getGrade());
+			}
+
+		});
+
+		for (int i = 0; i < enrollments.length; i++) {
 			Enrollment enrollment = enrollments[i];
 			if (enrollment.getCourse().contains("COMPSCI")) {
 				finalEnrollment[counter] = enrollment;
 				counter++;
-				
-				
+
 			}
-				
+
 		}
 		return finalEnrollment;
 	}
 
 	public Enrollment[] sortedStudentsInFileThree() {
-		int counter= 0;
+		int counter = 0;
 		FileService fileService = new FileService();
 		Enrollment[] enrollments = fileService.getStudentsFromFile();
 		Enrollment[] finalEnrollment = new Enrollment[33];
-		
-		//Arrays.sort(enrollments);
-		Arrays.sort(enrollments,new Comparator<Enrollment>() {
+
+		// Arrays.sort(enrollments);
+		Arrays.sort(enrollments, new Comparator<Enrollment>() {
 
 			@Override
 			public int compare(Enrollment o1, Enrollment o2) {
-				
-				return o1.getGrade().compareTo(o2.getGrade());
+
+				return o2.getGrade().compareTo(o1.getGrade());
 			}
-			
+
 		});
 
-		for (int i = 0; i<enrollments.length;i++) {
+		for (int i = 0; i < enrollments.length; i++) {
 			Enrollment enrollment = enrollments[i];
 			if (enrollment.getCourse().contains("STAT")) {
 				finalEnrollment[counter] = enrollment;
 				counter++;
-				
-				
+
 			}
-				
+
 		}
 		return finalEnrollment;
 	}
@@ -158,9 +124,8 @@ public void writeToAFile(String fileName, Enrollment[] enrollments)throws IOExce
 		String outputFile3 = "course3.csv";
 
 		try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
-				BufferedWriter writer1 = new BufferedWriter(new FileWriter(outputFile1));
-				BufferedWriter writer2 = new BufferedWriter(new FileWriter(outputFile2));
-				BufferedWriter writer3 = new BufferedWriter(new FileWriter(outputFile3))) {
+
+		) {
 //		 String header = reader.readLine();
 //            if (header != null) {
 //                writer1.write(header);
@@ -171,8 +136,8 @@ public void writeToAFile(String fileName, Enrollment[] enrollments)throws IOExce
 //                writer3.newLine();
 //                
 //               System.out.println(header);
- //           }
-				
+			// }
+
 			int i = 0;
 			Enrollment[] enrollments = new Enrollment[100];
 			String line = "";
@@ -200,46 +165,11 @@ public void writeToAFile(String fileName, Enrollment[] enrollments)throws IOExce
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-			// return;
-			// System.out.println(enrollment);
-
-			// Arrays.toString(enrollments);
-			// NEXT STEP SPLIT INTO SEPARATE COURSES!!!!!
-
-			// Store the students in a variable by courses using an if statement make a
-			// method
 
 		}
 
+		return null;
 
-return null;
+	}
 
 }
-
-}
-
-/*
- * Create file reader create file writer
- * 
- * split in to 3 separate files
- * 
- * You will need to parse the Master List File and separate the data into 3
- * separate CSV files. Each CSV file should contain a list of students specific
- * to that particular course (i.e. we're grouping the students by course).
- * 
- * For each of the 3 CSV files, you'll need to sort the students by grade in
- * descending order.
- * 
- * The CSV output file names should be called: course1.csv, course2.csv, and
- * course3.csv.
- * 
- */
-
-//public Enrollment getSortedClasses(Enrollment[] enrollments, String name, String course, Integer grade) {
-//	
-//	for(int i =0; i<enrollments.length; i++) {
-//		Enrollment studentEnrollments = enrollments[i];
-//		
-//		if(studentEnrollments.equals("COMSCI"){
-//			
-//		}
